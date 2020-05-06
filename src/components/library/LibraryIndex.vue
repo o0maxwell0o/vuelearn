937eb018c56361ae25176aeb100233bd7da7b06e
o0maxwell0o <40237850+o0maxwell0o@users.noreply.github.com>
2020-05-06 23:53:08 +0900

    library sql operation

--- src/components/library/LibraryIndex.vue
+++ src/components/library/LibraryIndex.vue
@@ -2,22 +2,33 @@
     <el-container>
         <el-aside style="width: 200px;margin-top: 20px">
             <switch></switch>
-            <SideMenu></SideMenu>
+            <SideMenu @indexSelect="listByCategory" ref="sideMenu"></SideMenu>
         </el-aside>
         <el-main>
-            <Books></Books>
+            <books class="books-area" ref="booksArea"></books>
         </el-main>
     </el-container>
 </template>
 
 <script>
-    import SideMenu from "./SideMenu";
-    import Books from "./Books";
+    import SideMenu from './SideMenu'
+    import Books from './Books'
 
     export default {
-        name: 'LibraryIndex',
-        components: {SideMenu,Books}
-
+        name: 'AppLibrary',
+        components: {Books, SideMenu},
+        methods: {
+            listByCategory () {
+                var _this = this
+                var cid = this.$refs.sideMenu.cid
+                var url = 'categories/' + cid + '/books'
+                this.$axios.get(url).then(resp => {
+                    if (resp && resp.status === 200) {
+                        _this.$refs.booksArea.books = resp.data
+                    }
+                })
+            }
+        }
     }
 </script>
 
