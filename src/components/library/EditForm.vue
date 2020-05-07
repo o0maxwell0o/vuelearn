ccdef61688f575083d2a213bcfd7b2690689023f
o0maxwell0o <40237850+o0maxwell0o@users.noreply.github.com>
2020-05-07 22:17:50 +0900

    image upload

--- src/components/library/EditForm.vue
+++ src/components/library/EditForm.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <i class="el-icon-circle-plus-outline"  @click="dialogFormVisible = true"></i>
+        <i class="el-icon-circle-plus-outline" @click="dialogFormVisible = true"></i>
         <el-dialog
                 title="添加/修改图书"
                 :visible.sync="dialogFormVisible"
@@ -20,6 +20,7 @@
                 </el-form-item>
                 <el-form-item label="封面" :label-width="formLabelWidth" prop="cover">
                     <el-input v-model="form.cover" autocomplete="off" placeholder="图片 URL"></el-input>
+                    <img-upload @onUpload="uploadImg" ref="imgUpload"></img-upload>
                 </el-form-item>
                 <el-form-item label="简介" :label-width="formLabelWidth" prop="abs">
                     <el-input type="textarea" v-model="form.abs" autocomplete="off"></el-input>
@@ -47,9 +48,12 @@
 </template>
 
 <script>
+    import ImgUpload from "../ImgUpload";
+
     export default {
         name: 'EditForm',
-        data () {
+        components: {ImgUpload},
+        data() {
             return {
                 dialogFormVisible: false,
                 form: {
@@ -69,7 +73,8 @@
             }
         },
         methods: {
-            clear () {
+            clear() {
+                this.$refs.imgUpload.clear()
                 this.form = {
                     id: '',
                     title: '',
@@ -84,7 +89,7 @@
                     }
                 }
             },
-            onSubmit () {
+            onSubmit() {
                 this.$axios
                     .post('/books', {
                         id: this.form.id,
@@ -101,6 +106,9 @@
                         this.$emit('onSubmit')
                     }
                 })
+            },
+            uploadImg() {
+                this.form.cover = this.$refs.imgUpload.url
             }
         }
     }
